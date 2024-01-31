9c30590a1b3e2217c2ad915d179daa15bdd2ceb6
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-31 10:39:18 +0100

    Update ModBlockLootProvider.java

--- 1.20.4/Common/src/main/java/fuzs/tradingpost/data/ModBlockLootProvider.java
+++ 1.20.4/Common/src/main/java/fuzs/tradingpost/data/ModBlockLootProvider.java
@@ -5,6 +5,7 @@ import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
 import fuzs.tradingpost.init.ModRegistry;
 
 public class ModBlockLootProvider extends AbstractLootProvider.Blocks {
+    
     public ModBlockLootProvider(DataProviderContext context) {
         super(context);
     }
