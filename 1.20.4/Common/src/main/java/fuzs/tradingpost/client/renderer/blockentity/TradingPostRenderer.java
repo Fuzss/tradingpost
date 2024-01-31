e6b01a86d5d9c331e18a36f53d9f5b7176ef6995
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-31 10:15:53 +0100

    migrate animation logic to own class

--- 1.20.4/Common/src/main/java/fuzs/tradingpost/client/renderer/blockentity/TradingPostRenderer.java
+++ 1.20.4/Common/src/main/java/fuzs/tradingpost/client/renderer/blockentity/TradingPostRenderer.java
@@ -2,7 +2,9 @@ package fuzs.tradingpost.client.renderer.blockentity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import fuzs.tradingpost.world.level.block.entity.TradingPostAnimationController;
 import fuzs.tradingpost.world.level.block.entity.TradingPostBlockEntity;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
@@ -17,6 +19,8 @@ import net.minecraft.world.level.Level;
  * Mostly copied from Quark's <a href="https://github.com/VazkiiMods/Quark/blob/master/src/main/java/vazkii/quark/addons/oddities/client/render/be/MatrixEnchantingTableRenderer.java">MatrixEnchantingTableRenderer.java</a> by Vazkii, thanks!
  */
 public class TradingPostRenderer implements BlockEntityRenderer<TradingPostBlockEntity> {
+    private static final ItemStack ITEM_STACK = new ItemStack(Items.EMERALD);
+
     private final ItemRenderer itemRenderer;
 
     public TradingPostRenderer(BlockEntityRendererProvider.Context context) {
@@ -24,33 +28,55 @@ public class TradingPostRenderer implements BlockEntityRenderer<TradingPostBlock
     }
 
     @Override
-    public void render(TradingPostBlockEntity blockEntity, float partialTicks, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
-        float ageInTicks = blockEntity.time + partialTicks;
-        float nextRotation = blockEntity.rot - blockEntity.oRot;
+    public void render(TradingPostBlockEntity blockEntity, float partialTick, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, int packedOverlay) {
+        // light is normally always 0 since it checks inside the crafting table block which is solid, but contents are rendered in the block above
+        packedLight = blockEntity.getLevel() != null ?
+                LevelRenderer.getLightColor(blockEntity.getLevel(), blockEntity.getBlockPos().above()) :
+                15728880;
+        TradingPostAnimationController animationController = blockEntity.getAnimationController();
+        float ageInTicks = animationController.time + partialTick;
+        float nextRotation = animationController.rot - animationController.oRot;
         while (nextRotation >= Math.PI) {
-            nextRotation -= (Math.PI * 2F);
+            nextRotation -= (float) (Math.PI * 2F);
         }
         while (nextRotation < -Math.PI) {
-            nextRotation += (Math.PI * 2F);
+            nextRotation += (float) (Math.PI * 2F);
         }
-        float bookRotation = blockEntity.oRot + nextRotation * partialTicks;
-        float bookOpen = Mth.lerp(partialTicks, blockEntity.oOpen, blockEntity.open);
-        this.renderItem(new ItemStack(Items.EMERALD), ageInTicks, bookOpen, bookRotation, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn, blockEntity.getLevel());
+        float bookRotation = animationController.oRot + nextRotation * partialTick;
+        float bookOpen = Mth.lerp(partialTick, animationController.oOpen, animationController.open);
+        this.renderItem(ITEM_STACK,
+                ageInTicks,
+                bookOpen,
+                bookRotation,
+                poseStack,
+                multiBufferSource,
+                packedLight,
+                packedOverlay,
+                blockEntity.getLevel()
+        );
     }
 
-    private void renderItem(ItemStack stack, float ageInTicks, float bookOpen, float bookRotation, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn, Level level) {
-        matrixStackIn.pushPose();
-        matrixStackIn.translate(0.5F, 1.03125F, 0.5F);
-        matrixStackIn.scale(0.8F, 0.8F, 0.8F);
+    private void renderItem(ItemStack itemStack, float ageInTicks, float bookOpen, float bookRotation, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, int packedOverlay, Level level) {
+        poseStack.pushPose();
+        poseStack.translate(0.5F, 1.03125F, 0.5F);
+        poseStack.scale(0.8F, 0.8F, 0.8F);
         bookRotation *= -180.0F / (float) Math.PI;
         bookRotation -= 90.0F;
         bookRotation *= bookOpen;
-        matrixStackIn.mulPose(Axis.YP.rotationDegrees(bookRotation));
-        matrixStackIn.translate(0.0F, bookOpen, Math.sin(bookOpen * Math.PI));
-        matrixStackIn.mulPose(Axis.XP.rotationDegrees(-90.0F * (bookOpen - 1.0F)));
+        poseStack.mulPose(Axis.YP.rotationDegrees(bookRotation));
+        poseStack.translate(0.0F, bookOpen, Math.sin(bookOpen * Math.PI));
+        poseStack.mulPose(Axis.XP.rotationDegrees(-90.0F * (bookOpen - 1.0F)));
         float hoveringHeight = (float) Math.sin(ageInTicks * 0.06F) * bookOpen * 0.2F;
-        matrixStackIn.translate(0.0F, hoveringHeight, 0.0F);
-        this.itemRenderer.renderStatic(stack, ItemDisplayContext.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn, level, 0);
-        matrixStackIn.popPose();
+        poseStack.translate(0.0F, hoveringHeight, 0.0F);
+        this.itemRenderer.renderStatic(itemStack,
+                ItemDisplayContext.FIXED,
+                packedLight,
+                packedOverlay,
+                poseStack,
+                multiBufferSource,
+                level,
+                0
+        );
+        poseStack.popPose();
     }
 }
--- 1.20.4/Common/src/main/java/fuzs/tradingpost/world/level/block/entity/TradingPostBlockEntity.java
+++ 1.20.4/Common/src/main/java/fuzs/tradingpost/world/level/block/entity/TradingPostBlockEntity.java
@@ -5,9 +5,7 @@ import fuzs.tradingpost.init.ModRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.Mth;
 import net.minecraft.world.Nameable;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
@@ -15,16 +13,13 @@ import org.jetbrains.annotations.Nullable;
 public class TradingPostBlockEntity extends BlockEntity implements Nameable, TickingBlockEntity {
     public static final Component CONTAINER_COMPONENT = Component.translatable("container.trading_post");
 
+    private final TradingPostAnimationController animationController;
+    @Nullable
     private Component name;
-    public int time;
-    public float open;
-    public float oOpen;
-    public float rot;
-    public float oRot;
-    private float tRot;
 
     public TradingPostBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(ModRegistry.TRADING_POST_BLOCK_ENTITY_TYPE.value(), blockPos, blockState);
+        this.animationController = new TradingPostAnimationController(blockPos);
     }
 
     @Override
@@ -45,42 +40,11 @@ public class TradingPostBlockEntity extends BlockEntity implements Nameable, Tic
 
     @Override
     public void clientTick() {
-        if (this.getLevel() == null || !this.getLevel().isClientSide) return;
-        this.oOpen = this.open;
-        this.oRot = this.rot;
-        Player playerentity = this.getLevel().getNearestPlayer((double) this.getBlockPos().getX() + 0.5, (double) this.getBlockPos().getY() + 0.5, (double) this.getBlockPos().getZ() + 0.5, 3.0, false);
-        if (playerentity != null) {
-            double d0 = playerentity.getX() - ((double) this.getBlockPos().getX() + 0.5);
-            double d1 = playerentity.getZ() - ((double) this.getBlockPos().getZ() + 0.5);
-            this.tRot = (float) Mth.atan2(d1, d0);
-            this.open += 0.1F;
-        } else {
-            this.tRot += 0.02F;
-            this.open -= 0.1F;
-        }
-        while(this.rot >= (float) Math.PI) {
-            this.rot -= ((float) Math.PI * 2.0F);
-        }
-        while(this.rot < -(float) Math.PI) {
-            this.rot += ((float) Math.PI * 2.0F);
-        }
-        while(this.tRot >= (float) Math.PI) {
-            this.tRot -= ((float) Math.PI * 2.0F);
-        }
-        while(this.tRot < -(float) Math.PI) {
-            this.tRot += ((float) Math.PI * 2.0F);
-        }
-        float f2;
-        f2 = this.tRot - this.rot;
-        while (f2 >= (float) Math.PI) {
-            f2 -= ((float) Math.PI * 2.0F);
-        }
-        while(f2 < -(float) Math.PI) {
-            f2 += ((float) Math.PI * 2.0F);
-        }
-        this.rot += f2 * 0.4F;
-        this.open = Mth.clamp(this.open, 0.0F, 1.0F);
-        ++this.time;
+        this.animationController.tick(this.getLevel());
+    }
+
+    public TradingPostAnimationController getAnimationController() {
+        return this.animationController;
     }
 
     @Override
@@ -88,8 +52,8 @@ public class TradingPostBlockEntity extends BlockEntity implements Nameable, Tic
         return this.name != null ? this.name : CONTAINER_COMPONENT;
     }
 
-    public void setCustomName(@Nullable Component textComponent) {
-        this.name = textComponent;
+    public void setCustomName(@Nullable Component component) {
+        this.name = component;
     }
 
     @Nullable
